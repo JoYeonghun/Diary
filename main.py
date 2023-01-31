93c51c35afe4a1702214f1fe821216204b6bff32
Yeonghun Jo <117628869+JoYeonghun@users.noreply.github.com>
2023-01-31 16:53:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -52,85 +52,85 @@ bertmodel, vocab = get_pytorch_kobert_model()
 model = torch.load('https://github.com/JoYeonghun/Diary/tree/main/model')  # GPU 사용
 # model = torch.load('https://github.com/JoYeonghun/Diary/blob/main/7emotions_model.pt', map_location=torch.device('cpu'))  # CPU 사용
 
-## 4. 데이터 전처리(토큰화, 정수 인코딩, 패딩)
-class BERTDataset(Dataset):
-    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
-                 pad, pair):
-        transform = nlp.data.BERTSentenceTransform(
-            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+# ## 4. 데이터 전처리(토큰화, 정수 인코딩, 패딩)
+# class BERTDataset(Dataset):
+#     def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+#                  pad, pair):
+#         transform = nlp.data.BERTSentenceTransform(
+#             bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
 
-        self.sentences = [transform([i[sent_idx]]) for i in dataset]
-        self.labels = [np.int32(i[label_idx]) for i in dataset]
+#         self.sentences = [transform([i[sent_idx]]) for i in dataset]
+#         self.labels = [np.int32(i[label_idx]) for i in dataset]
 
-    def __getitem__(self, i):
-        return (self.sentences[i] + (self.labels[i], ))
+#     def __getitem__(self, i):
+#         return (self.sentences[i] + (self.labels[i], ))
 
-    def __len__(self):
-        return (len(self.labels))
+#     def __len__(self):
+#         return (len(self.labels))
 
-## Setting parameters
-max_len = 64
-batch_size = 64
-warmup_ratio = 0.1
-num_epochs = 5
-max_grad_norm = 1
-log_interval = 200
-learning_rate =  5e-5
+# ## Setting parameters
+# max_len = 64
+# batch_size = 64
+# warmup_ratio = 0.1
+# num_epochs = 5
+# max_grad_norm = 1
+# log_interval = 200
+# learning_rate =  5e-5
 
-## 8. 결과물 테스트
-## 토큰화
-tokenizer = get_tokenizer()
-tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+# ## 8. 결과물 테스트
+# ## 토큰화
+# tokenizer = get_tokenizer()
+# tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
 
-## 감정 예측
-def predict(predict_sentence):
+# ## 감정 예측
+# def predict(predict_sentence):
 
-    data = [predict_sentence, '0']
-    dataset_another = [data]
+#     data = [predict_sentence, '0']
+#     dataset_another = [data]
 
-    another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
-    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
+#     another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
+#     test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
     
-    model.eval()
+#     model.eval()
  
-    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
-        token_ids = token_ids.long().to(device)
-        segment_ids = segment_ids.long().to(device)
+#     for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+#         token_ids = token_ids.long().to(device)
+#         segment_ids = segment_ids.long().to(device)
 
-        valid_length= valid_length
-        label = label.long().to(device)
+#         valid_length= valid_length
+#         label = label.long().to(device)
 
-        out = model(token_ids, valid_length, segment_ids)
+#         out = model(token_ids, valid_length, segment_ids)
 
-        test_eval=[]
-        for i in out:
-            logits=i
-            logits = logits.detach().cpu().numpy()
+#         test_eval=[]
+#         for i in out:
+#             logits=i
+#             logits = logits.detach().cpu().numpy()
 
-            if np.argmax(logits) == 0:
-                test_eval.append("공포가")
-            elif np.argmax(logits) == 1:
-                test_eval.append("놀람이")
-            elif np.argmax(logits) == 2:
-                test_eval.append("분노가")
-            elif np.argmax(logits) == 3:
-                test_eval.append("슬픔이")
-            elif np.argmax(logits) == 4:
-                test_eval.append("중립이")
-            elif np.argmax(logits) == 5:
-                test_eval.append("행복이")
-            elif np.argmax(logits) == 6:
-                test_eval.append("혐오가")
+#             if np.argmax(logits) == 0:
+#                 test_eval.append("공포가")
+#             elif np.argmax(logits) == 1:
+#                 test_eval.append("놀람이")
+#             elif np.argmax(logits) == 2:
+#                 test_eval.append("분노가")
+#             elif np.argmax(logits) == 3:
+#                 test_eval.append("슬픔이")
+#             elif np.argmax(logits) == 4:
+#                 test_eval.append("중립이")
+#             elif np.argmax(logits) == 5:
+#                 test_eval.append("행복이")
+#             elif np.argmax(logits) == 6:
+#                 test_eval.append("혐오가")
 
-        return (test_eval[0] + " 느껴집니다.")
+#         return (test_eval[0] + " 느껴집니다.")
 
-## Text Input
-message = st.text_area("감정을 기록해주세요")
+# ## Text Input
+# message = st.text_area("감정을 기록해주세요")
 
-if st.button("기록", key='message'):
-    result = message.title()
-    st.success(result)
-    st.text("기록이 완료됐습니다.")
+# if st.button("기록", key='message'):
+#     result = message.title()
+#     st.success(result)
+#     st.text("기록이 완료됐습니다.")
 
-    ## 문장감정분석 출력
-    st.write(predict(result))
+#     ## 문장감정분석 출력
+#     st.write(predict(result))
